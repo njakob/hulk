2ce97d41eb6d84707208a6f4c6cb03b469df9b79
Nicolas Jakob <nicow.jakob@gmail.com>
2017-06-05 14:30:24 +0200

    feat: Add commit hash retrieval from Git command line

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 /* @flow */
 
+import * as childProcess from 'child_process';
+
+export function getCommitHash(): string {
+  return childProcess.execSync('git rev-parse HEAD').toString('utf8').trim();
+}
+
 type HulkOptions = {
   name: string;
   version: string;
